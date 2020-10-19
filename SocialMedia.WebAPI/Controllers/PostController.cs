23f7b4668290273e1865c1246e14a5de2a7cd528
lyankey <56731939+lyankey@users.noreply.github.com>
2020-10-19 17:32:23 -0400

    a skeleton to start with

---
+++ SocialMedia.WebAPI/Controllers/PostController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMedia.WebAPI.Controllers
+{
+    public class PostController : ApiController
+    {
+        public IHttpActionResult Get(int id)
+        {
+            PostService postService = CreatePostService();
+            var note = postService.GetPostsById(id);
+            return Ok(note);
+        }
+        public IHttpActionResult Post(CPost post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var notes = postService.GetAllPosts();
+            return Ok(notes);
+        }
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new PostService(userId);
+            return noteService;
+        }
+        public IHttpActionResult Put(PostEdit post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.UpdateNote(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePostService();
+
+            if (!service.DeletePost(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
