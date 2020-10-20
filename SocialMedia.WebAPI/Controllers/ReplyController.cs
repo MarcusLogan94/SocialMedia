f70c2ee185531a87bce6ddf62c2618382b11ae06
jacobschilders <jschilders@anderson.edu>
2020-10-20 14:13:31 -0400

    reply update

---
+++ SocialMedia.WebAPI/Controllers/ReplyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMedia.WebAPI.Controllers
+{
+    public class ReplyController : ApiController
+    {
+        public IHttpActionResult Post(ReplyCreate reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.CreateReply(reply))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private ReplyService CreateReplyService()
+        {
+            var replyId = Guid.Parse(User.Identity.GetUserId());
+            var replyService = new ReplyService(replyId);
+            return replyService;
+        }
+    }
+}
